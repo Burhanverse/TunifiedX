d2c28d1384305ab29487c2a968e31c765ce909f6
Burhanverse <burhanverse@gmail.com>
2024-08-28 23:22:30 +0530

    Bring back set and unset cmds

--- bot.mjs
+++ bot.mjs
@@ -1,7 +1,7 @@
 import { Telegraf, Markup } from 'telegraf';
 import LastFmNode from 'lastfmapi';
 import { fetchSpotifyAlbumArt } from './src/spotify.mjs';
-import { getUserLastfmUsername } from './src/utils.mjs';
+import { getUserLastfmUsername, setUserLastfmUsername, unsetUserLastfmUsername } from './src/utils.mjs';
 import dotenv from 'dotenv';
 import moment from 'moment';
 import path from 'path';
@@ -19,65 +19,89 @@ const lastfm = new LastFmNode({
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-bot.command('status', async (ctx) => {
+bot.command('set', async (ctx) => {
+    const username = ctx.message.text.split(' ')[1];
+    if (!username) {
+        return ctx.reply('Please provide a Last.fm username like /set <username>.');
+    }
+    await setUserLastfmUsername(ctx.from.id, username);
+    ctx.reply(`Username set to ${username}`);
+});
+
+bot.command('unset', async (ctx) => {
     const username = await getUserLastfmUsername(ctx.from.id);
     if (!username) {
-        return ctx.reply('You need to set your Last.fm username first using /set.');
+        return ctx.reply('No username found to unlink. Use /set <username> first.');
     }
+    await unsetUserLastfmUsername(ctx.from.id);
+    ctx.reply('Username unlinked.');
+});
 
-    lastfm.user.getRecentTracks({ user: username, limit: 1 }, async (err, data) => {
-        if (err || data.track.length === 0) {
-            return ctx.reply('No recent tracks found.');
+bot.command('status', async (ctx) => {
+    try {
+        const username = await getUserLastfmUsername(ctx.from.id);
+        if (!username) {
+            return ctx.reply('You need to set your Last.fm username first using /set.');
         }
 
-        const recentTrack = data.track[0];
-        const trackName = recentTrack.name;
-        const artistName = recentTrack.artist['#text'];
-        const albumName = recentTrack.album['#text'] || 'Unknown Album';
-        const trackMbid = recentTrack.mbid;
-
-        // Determine if the track is currently playing
-        const isPlaying = recentTrack['@attr'] && recentTrack['@attr'].nowplaying === 'true';
-        const status = isPlaying ? 'Playing' : 'Paused';
-
-        lastfm.track.getInfo({
-            artist: artistName,
-            track: trackName,
-            username: username,
-            mbid: trackMbid
-        }, async (err, trackInfo) => {
-            if (err || !trackInfo) {
-                return ctx.reply('Could not fetch track info.');
+        lastfm.user.getRecentTracks({ user: username, limit: 1 }, async (err, data) => {
+            if (err || !data.track || data.track.length === 0) {
+                console.error('Error fetching recent tracks:', err || 'No recent tracks found');
+                return ctx.reply('No recent tracks found.');
             }
 
-            const playCount = trackInfo.userplaycount || 'N/A';
-            const lastPlayed = recentTrack.date ? 
-                moment.unix(recentTrack.date.uts).format('DD/MM/YYYY HH:mm:ss') : 
-                moment().format('DD/MM/YYYY HH:mm:ss');  // Use current time for now playing
+            const recentTrack = data.track[0];
+            const trackName = recentTrack.name;
+            const artistName = recentTrack.artist['#text'];
+            const albumName = recentTrack.album['#text'] || 'Unknown Album';
+            const trackMbid = recentTrack.mbid || null;
 
-            const albumArt = await fetchSpotifyAlbumArt(albumName);
-            const songLinkId = trackInfo.mbid || trackInfo.url.split('/').pop();
+            const isPlaying = recentTrack['@attr'] && recentTrack['@attr'].nowplaying === 'true';
+            const status = isPlaying ? 'Playing' : 'Paused';
 
-            const response = `<b>${ctx.from.first_name} ${ctx.from.last_name ? ctx.from.last_name : ''} is Listening to:</b>\n\n` +
-                `<b>Song:</b> ${trackName}\n` +
-                `<b>Artist:</b> ${artistName}\n` +
-                `<b>Album:</b> ${albumName}\n` +
-                `<b>Play Count:</b> ${playCount}\n` +
-                `<b>Status:</b> ${status}\n` +
-                `<b>Last Played:</b> ${lastPlayed}`;
+            lastfm.track.getInfo({
+                artist: artistName,
+                track: trackName,
+                username: username,
+                mbid: trackMbid
+            }, async (err, trackInfo) => {
+                if (err || !trackInfo) {
+                    console.error('Error fetching track info:', err || 'No track info available');
+                    return ctx.reply('Could not fetch track info.');
+                }
 
-            const buttons = Markup.inlineKeyboard([
-                Markup.button.url('Made by AquaMods', 'https://akuamods.t.me')
-            ]);
+                const playCount = trackInfo.userplaycount || 'N/A';
+                const lastPlayed = recentTrack.date ? 
+                    moment.unix(recentTrack.date.uts).format('DD/MM/YYYY HH:mm:ss') : 
+                    moment().format('DD/MM/YYYY HH:mm:ss');
 
-            if (albumArt.includes('http')) {
-                ctx.replyWithPhoto({ url: albumArt }, { caption: response, parse_mode: 'HTML', ...buttons });
-            } else {
-                const imagePath = path.join(__dirname, albumArt);
-                ctx.replyWithPhoto({ source: imagePath }, { caption: response, parse_mode: 'HTML', ...buttons });
-            }
+                const albumArt = await fetchSpotifyAlbumArt(albumName);
+                const songLinkId = trackInfo.mbid || trackInfo.url.split('/').pop();
+
+                const response = `<b>${ctx.from.first_name} ${ctx.from.last_name || ''} is Listening to:</b>\n\n` +
+                    `<b>Song:</b> ${trackName}\n` +
+                    `<b>Artist:</b> ${artistName}\n` +
+                    `<b>Album:</b> ${albumName}\n` +
+                    `<b>Play Count:</b> ${playCount}\n` +
+                    `<b>Status:</b> ${status}\n` +
+                    `<b>Last Played:</b> ${lastPlayed}`;
+
+                const buttons = Markup.inlineKeyboard([
+                    Markup.button.url('Made by AquaMods', 'https://akuamods.t.me')
+                ]);
+
+                if (albumArt.includes('http')) {
+                    ctx.replyWithPhoto({ url: albumArt }, { caption: response, parse_mode: 'HTML', ...buttons });
+                } else {
+                    const imagePath = path.join(__dirname, albumArt);
+                    ctx.replyWithPhoto({ source: imagePath }, { caption: response, parse_mode: 'HTML', ...buttons });
+                }
+            });
         });
-    });
+    } catch (error) {
+        console.error('Error processing status command:', error);
+        ctx.reply('An error occurred while processing your request.');
+    }
 });
 
 bot.launch();
