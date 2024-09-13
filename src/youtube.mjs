e9d4ecdcdad330e5edadfa81fed548d1c8a36cd5
Burhanverse <burhanverse@gmail.com>
2024-09-13 13:14:46 +0530

    simplify the youtube albumart function

--- src/youtube.mjs
+++ src/youtube.mjs
@@ -6,16 +6,13 @@ dotenv.config();
 export async function getYouTubeAlbumArt(artist, track) {
     const searchString = `${artist} ${track}`;
     try {
-        // Search on YouTube for the track
         const videoInfo = await ytdl.getInfo(`ytsearch1:${searchString}`);
         const videoDetails = videoInfo.videoDetails;
 
-        if (videoDetails && videoDetails.thumbnails && videoDetails.thumbnails.length > 0) {
-            // Return the highest resolution thumbnail available
-            return videoDetails.thumbnails[videoDetails.thumbnails.length - 1].url;
-        }
+        return {
+            albumArtUrl: videoDetails.thumbnails[0].url
+        };
 
-        return null;
     } catch (error) {
         console.error('YouTube Music API error:', error);
         return null;
